efc91e9c818e1555fcdbe34cfbe81168585b74cc
Bruna Soares <brunaeloisa7@gmail.com>
2026-06-15 01:52:22 +0000

    feat(desktop): limit shortcut selection layer to icon shape

--- src/components/Shortcut.tsx
+++ src/components/Shortcut.tsx
@@ -13,6 +13,8 @@ export function Shortcut({
   onOpen,
   onSelect
 }: ShortcutProps) {
+  const filterId = `filter-${name.toLowerCase().replaceAll(' ', '-')}`;
+
   return (
     <button
       type="button"
@@ -20,8 +22,34 @@ export function Shortcut({
       onClick={onSelect}
       onDoubleClick={onOpen}
     >
-      <div className={isSelected ? 'selected-icon' : ''}>
-        <img src={icon} alt={name} className="block w-16" />
+      <div className="relative">
+        <svg className="invisible absolute h-0 w-0">
+          <defs>
+            <filter id={filterId}>
+              <feFlood
+                floodColor="var(--color-active)"
+                floodOpacity="0.6"
+                result="selectionLayer"
+              />
+
+              <feComposite
+                in="selectionLayer"
+                in2="SourceGraphic"
+                operator="in"
+                result="mask"
+              />
+
+              <feBlend mode="normal" in="mask" in2="SourceGraphic" />
+            </filter>
+          </defs>
+        </svg>
+
+        <img
+          src={icon}
+          alt={name}
+          className="block w-16"
+          style={{ filter: isSelected ? `url(#${filterId})` : 'none' }}
+        />
       </div>
 
       <span
