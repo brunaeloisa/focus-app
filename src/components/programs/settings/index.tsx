26ea6cd7329f096b7c8dc1d191a50c1b5a6a33f8
bruna <brunaeloisa7@gmail.com>
2026-06-27 23:20:39 -0300

    feat(settings): customize theme select to match the ui

--- src/components/programs/settings/index.tsx
+++ src/components/programs/settings/index.tsx
@@ -4,6 +4,7 @@ import { THEMES_DATA } from '../../../data/themes';
 import { useState } from 'react';
 import { Button3D } from '../../ui/Button3D';
 import { ThemePreview } from './ThemePreview';
+import { CustomSelect } from '../../ui/CustomSelect';
 
 export function Settings() {
   const dispatch = useAppDispatch();
@@ -11,17 +12,28 @@ export function Settings() {
   const [preview, setPreview] = useState(currentTheme);
 
   return (
-    <>
-      <ThemePreview theme={preview} />
-      <div>Escolha um tema: </div>
-      <select value={preview} onChange={(e) => setPreview(e.target.value)}>
-        {THEMES_DATA.map((theme) => (
-          <option key={theme} value={theme}>
-            {theme}
-          </option>
-        ))}
-      </select>
-      <Button3D onClick={() => dispatch(setTheme(preview))}>Aplicar</Button3D>
-    </>
+    <div className="flex flex-col gap-1.5 px-6 py-8">
+      <div className="self-center">
+        <ThemePreview theme={preview} />
+      </div>
+
+      <label htmlFor=" theme-select">Escolha um tema: </label>
+
+      <div className="flex w-full items-stretch gap-2">
+        <CustomSelect
+          id="theme-select"
+          value={preview}
+          onChange={(e) => setPreview(e.target.value)}
+          options={THEMES_DATA}
+        />
+
+        <Button3D
+          className="px-2.5 py-px"
+          onClick={() => dispatch(setTheme(preview))}
+        >
+          Aplicar
+        </Button3D>
+      </div>
+    </div>
   );
 }
